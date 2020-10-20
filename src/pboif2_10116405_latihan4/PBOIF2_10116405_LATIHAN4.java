5a3308c3a11c579174733307a88b644ebd7d8bb8
Fahrih Fatahilah <painchikusudo@gmail.com>
2020-10-20 12:18:03 +0800

    PBOIF2

---
+++ src/pboif2_10116405_latihan4/PBOIF2_10116405_LATIHAN4.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116405_latihan4;
+
+/**
+ *
+ * @author Fahrih Fatahilah
+ * Nama  : Fahrih Fatahilah
+ * Kelas : IF-2 / PBO-IF2
+ * Nim   : 10116405
+ * 
+ */
+public class PBOIF2_10116405_LATIHAN4 {
+ public void tambahKambing(){
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah: " + jumlahKambing);
+    } 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PBOIF2_10116405_LATIHAN4 kambingJantan = new PBOIF2_10116405_LATIHAN4();
+        kambingJantan.tambahKambing();
+    }
+    
+}
